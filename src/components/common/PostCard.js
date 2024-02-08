d4f7fe5217709c395b859a8418ecb18704b01586
noahbershatsky <100739669+noahbershatsky@users.noreply.github.com>
2024-02-07 19:59:23 -0800

    Update PostCard.js

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -25,7 +25,7 @@ const PostCard = ({ post }) => {
                         <Tags
                             post={post}
                             visibility="public"
-                            autolink={false}
+                            autolink={true}
                         />
                     </div>
                 )}
@@ -35,24 +35,6 @@ const PostCard = ({ post }) => {
             <section className="post-card-excerpt">{post.excerpt}</section>
             <footer className="post-card-footer">
                 <div className="post-card-footer-left">
-                    <div className="post-card-avatar">
-                        {post.primary_author.profile_image ? (
-                            <img
-                                className="author-profile-image"
-                                src={post.primary_author.profile_image}
-                                alt={post.primary_author.name}
-                            />
-                        ) : (
-                            <img
-                                className="default-avatar"
-                                src="/images/icons/avatar.svg"
-                                alt={post.primary_author.name}
-                            />
-                        )}
-                    </div>
-                    <span>{post.primary_author.name}</span>
-                </div>
-                <div className="post-card-footer-right">
                     <div>{readingTime}</div>
                 </div>
             </footer>
